79898d5dda36d04fd2a37ad888cfb4b807f33631
lampogriz <potolochni@gmail.com>
2020-10-04 22:27:24 +0300

    Updates

---
+++ precache-manifest.7a767fb376659a740266c702ee9bc9b5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c843ecf95a332028a85be675422fa97b",
+    "url": "/React-exercise/index.html"
+  },
+  {
+    "revision": "956fcb7572f521a36b75",
+    "url": "/React-exercise/static/css/3.d37c0c22.chunk.css"
+  },
+  {
+    "revision": "5a73fb750fc73def4aab",
+    "url": "/React-exercise/static/css/main.d84ed8c0.chunk.css"
+  },
+  {
+    "revision": "affe9de8713b4258d506",
+    "url": "/React-exercise/static/js/2.af723089.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-exercise/static/js/2.af723089.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "956fcb7572f521a36b75",
+    "url": "/React-exercise/static/js/3.43edad8d.chunk.js"
+  },
+  {
+    "revision": "5a73fb750fc73def4aab",
+    "url": "/React-exercise/static/js/main.21a7eba2.chunk.js"
+  },
+  {
+    "revision": "268239b81cb2a5a10b43",
+    "url": "/React-exercise/static/js/runtime-main.b7b6f19d.js"
+  },
+  {
+    "revision": "1a7925ff96284abba19787786c047eb5",
+    "url": "/React-exercise/static/media/preloader.1a7925ff.gif"
+  }
+]);
